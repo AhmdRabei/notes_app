59cccbffb7ea638de958b40c261306ec5ecfb646
Ahmed Rabei <rabei51197@gmail.com>
2024-03-14 12:42:13 +0200

    create custom text field

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(
+            color: kPrimaryColor,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(
+            color: kPrimaryColor,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(
+            color: kPrimaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
