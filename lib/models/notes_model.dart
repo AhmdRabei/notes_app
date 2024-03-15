a0478df3e7066420ab8cfdc3f16cc6d2eb59a517
Ahmed Rabei <rabei51197@gmail.com>
2024-03-15 13:52:22 +0200

    update note model

--- lib/models/notes_model.dart
+++ lib/models/notes_model.dart
@@ -2,8 +2,10 @@ class NotesModel {
   final String title;
   final String subTitle;
   final String date;
+  final int color;
 
   NotesModel({
+    required this.color,
     required this.title,
     required this.subTitle,
     required this.date,
