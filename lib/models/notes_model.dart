e3520450a13288c15f4d2a3b2986a6ea395719b5
Ahmed Rabei <rabei51197@gmail.com>
2024-03-15 13:47:43 +0200

    create notes model

---
+++ lib/models/notes_model.dart
@@ -0,0 +1,11 @@
+class NotesModel {
+  final String title;
+  final String subTitle;
+  final String date;
+
+  NotesModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+  });
+}
--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/edit_view_body.dart';
 
 class EditNoteView extends StatelessWidget {
