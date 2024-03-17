90d8facbe18cb221ea0bd8323155ec754fa9ae2d
Ahmed Rabei <rabei51197@gmail.com>
2024-03-17 14:28:51 +0200

    take input and validate data

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -2,36 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
-class AddNoteForm extends StatelessWidget {
+class AddNoteForm extends StatefulWidget {
   const AddNoteForm({
     super.key,
   });
 
   @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        SizedBox(
-          height: 30,
-        ),
-        CustomTextField(
-          title: 'Title',
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        CustomTextField(
-          title: 'Content',
-          maxLines: 5,
-        ),
-        SizedBox(
-          height: 50,
-        ),
-        CustomButton(buttonTitle: 'Add Note'),
-        SizedBox(
-          height: 25,
-        ),
-      ],
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
+            title: 'Title',
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomTextField(
+            onSaved: (value) {
+              subTitle = value;
+            },
+            title: 'Content',
+            maxLines: 5,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomButton(
+            buttonTitle: 'Add Note',
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.buttonTitle});
+  const CustomButton({super.key, required this.buttonTitle, this.onTap});
   final String buttonTitle;
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: onTap,
       child: Container(
         height: 45,
         decoration: BoxDecoration(
