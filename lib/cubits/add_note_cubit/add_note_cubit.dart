0fc84473e94f091836852586f1249118e7107f81
Ahmed Rabei <rabei51197@gmail.com>
2024-03-26 22:17:50 +0200

    finsh add note bottom sheet

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_states.dart';
@@ -6,9 +9,10 @@ import 'package:notes_app/models/notes_model.dart';
 
 class AddNotesCubit extends Cubit<AddNoteStates> {
   AddNotesCubit() : super(AddNoteInatial());
-
+  Color color = const Color(0xff071E21);
   final List<NotesModel> notes = [];
   addNote(NotesModel note) async {
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var noteBox = Hive.box<NotesModel>(kNotesBox);
--- lib/widgets/colors_list_view.dart
+++ lib/widgets/colors_list_view.dart
@@ -1,32 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
+  const ColorItem({super.key, required this.color, required this.isActive});
+  final Color color;
+  final bool isActive;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(right: 8),
-      child: CircleAvatar(
-        radius: 25,
-        backgroundColor: Color.fromARGB(255, 4, 234, 234),
-      ),
-    );
+    return isActive
+        ? CircleAvatar(
+            radius: 34,
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+            child: CircleAvatar(
+              radius: 32,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 34,
+            backgroundColor: color,
+          );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
   @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+
+  List<Color> colorsList = const [
+    Color(0xff0E3B43),
+    Color(0xff357266),
+    Color(0xffA3BBAD),
+    Color(0xff65532F),
+    Color.fromARGB(255, 255, 217, 128),
+  ];
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 25 * 2,
+      height: 34 * 2,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 10,
+        itemCount: colorsList.length,
         itemBuilder: (context, index) {
-          return const ColorItem();
+          return GestureDetector(
+            onTap: () {
+              currentIndex = index;
+              BlocProvider.of<AddNotesCubit>(context).color = colorsList[index];
+              setState(() {});
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: ColorItem(
+                isActive: currentIndex == index,
+                color: colorsList[index],
+              ),
+            ),
+          );
         },
       ),
     );
